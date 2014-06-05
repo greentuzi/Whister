6df751eba90d17dd7c620c11f14b463a45e3f7e1
Greentuzi <greentuzi@gmail.com>
2014-06-05 18:00:50 +0800

    Bate 0.1
    前端的基本框架

---
+++ app/src/main/java/com/UnicornStudio/whiter/app/MainActivity.java
@@ -0,0 +1,68 @@
+package com.UnicornStudio.whiter.app;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    // Intent to Impression wall activity.
+    private Button iWallBt;
+
+    // View
+    private void findView(){
+        iWallBt = (Button) findViewById(R.id.impressionWallButton);
+    }
+
+    // Controller
+    private void setListenser(){
+        iWallBt.setOnClickListener(clikIwallBt);
+    }
+
+    private Button.OnClickListener clikIwallBt = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this,ImpressionWall.class);
+            startActivity(intent);
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findView();
+        setListenser();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
